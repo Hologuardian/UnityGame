37ee7152889db076199400a3d350dc43f31bc388
Hologuardian <namaeskram@gmail.com>
2015-10-09 10:26:47 -0400

    First person controller, have a wall, and a mountain.

---
+++ 3rd Person Test/Assets/Scripts/TestScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	public float speed = 20;
+	public Rigidbody projectile;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1")){
+			Debug.Log(transform.rotation.x + " " + transform.rotation.y + "" + transform.rotation.z + " " + transform.rotation.w);
+			Rigidbody instantiateProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+			instantiateProjectile.velocity = transform.TransformDirection(0, 0, speed);
+			Destroy(instantiateProjectile.gameObject, 30.0f);
+		}
+	}
+}
